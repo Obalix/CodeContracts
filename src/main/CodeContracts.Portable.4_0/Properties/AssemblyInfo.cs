f19253afd1b3dc6cfaa6189ab18cb9e984e5fc85
Obalix <axel@eckenberger.de>
2015-04-21 13:09:34 +0200

    Moved project folders under main. Created nuspec file for portable version.

--- src/main/CodeContracts.Portable.4_0/Properties/AssemblyInfo.cs
+++ src/main/CodeContracts.Portable.4_0/Properties/AssemblyInfo.cs
@@ -6,15 +6,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeContracts.Portable.4_5")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CodeContracts.Portable")]
+[assembly: AssemblyDescription("Portable Simple Code Contracts for every day!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeContracts.Portable.4_5")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Axel Eckenberger")]
+[assembly: AssemblyProduct("CodeContracts.Portalbe")]
+[assembly: AssemblyCopyright("Copyright © UBS AG 2014, Axel Eckenberger 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
